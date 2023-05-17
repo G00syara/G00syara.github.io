734c96410a63286efc9c45be4be2854eb688bfaa
G00syara <sasha.ugarov.02@mail.ru>
2023-05-18 00:34:48 +0300

    compete 3 lvl

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const hashPageTittle = "JS SPA HASh-Rou";
+
+const hashRouters= {
+    '/': '/views/main.html',
+    map: '/views/map.html',
+    timer: '/views/timer.html',
+};
+
+const hashHandleLocation = async() =>{
+    let location = window.location.hash.replace("#", ""); 
+    
+    if (location.length == 0){
+        location = "/";
+    } 
+
+    const route = hashRouters[location];
+    const html = await fetch(route).then((response)=> response.text());
+    if(location === "map") setTimeout(() => {ymaps.ready(init)}, 1000)
+    document.getElementById('app').innerHTML=html;
+
+}
+
+window.addEventListener("hashchange", hashHandleLocation)
+window.addEventListener("popstate", hashHandleLocation);
+hashHandleLocation();
+
+
--- js/maps.js
+++ js/maps.js
@@ -16,4 +16,3 @@ function init(){
 }
 
 
-ymaps.ready(init)
